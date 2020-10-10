8acb70bad957c699932fcead17cf31272f1c03f7
Gustavo Lopez <gml.gustavoml@gmail.com>
2020-10-10 19:26:56 -0300

    Was renamed the build folder to docs

---
+++ docs/precache-manifest.061a9d8abe5a5fed0ef7d40adb04e0bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef425ed503318b6943c01bf3a808cc99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25b9367fdd2db549a5c9",
+    "url": "/static/css/main.8c1929b5.chunk.css"
+  },
+  {
+    "revision": "d05f2f690261a011b0d7",
+    "url": "/static/js/2.0ffa3c8d.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.0ffa3c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b9367fdd2db549a5c9",
+    "url": "/static/js/main.eeaa9e0f.chunk.js"
+  },
+  {
+    "revision": "d5dfef93511362067405",
+    "url": "/static/js/runtime-main.768c873c.js"
+  }
+]);
